02b9dcfbe2d4c52e1fc0a3064fd30f87bb310a7c
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2020-09-12 19:57:30 -0700

    fourth-commit

--- week3/creating_a_list.js
+++ week3/creating_a_list.js
@@ -2,7 +2,7 @@ var h1 = document.createElement("h1")
 h1.textContent = "Hello World"
 document.body.append(h1)
  
-function fiona (){
+function tenTimes (){
     for (var i = 0; i < 10; i++){
         var h1 = document.createElement("h1")
         h1.textContent = "Hello World"
@@ -12,12 +12,12 @@ function fiona (){
     }
 }
 
-fiona()
+tenTimes()
 
 
 const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"]
 
-function shrek (){
+function list (){
     for (i = 0; i < names.length; i++){
         console.log(names[i])
         var donkey =  document.createElement("p")
@@ -26,7 +26,7 @@ function shrek (){
     }
 }
 
-shrek()
+list()
 
 
 
