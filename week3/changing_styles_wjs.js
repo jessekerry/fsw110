693984c30f3d87fc795d7bbe76c8a56cfbbb69c1
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2020-09-13 21:31:14 -0700

    fifth-commit

--- week3/changing_styles_wjs.js
+++ week3/changing_styles_wjs.js
@@ -1,6 +1,7 @@
 function farquad (){
     for ( i = 0; i < 5; i++){
         const h1 = document.createElement("h1")
+        h1.className = "border"
         h1.textContent = "Fairy Tale Ending"
         h1.style.fontSize ="20px"
         h1.style.fontWeight = "lighter"
